02d43975bbd83971f56be74c3568cdbc55bb4b7d
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2023-01-04 16:48:40 +0100

    minor UX to the show-graph script

--- scripts/show_humans_graph.py
+++ scripts/show_humans_graph.py
@@ -2,13 +2,19 @@ import graphviz
 import rospy
 from std_msgs.msg import String
 
+PATH = "/tmp/graph.pdf"
+
 
 def callback(msg):
     src = graphviz.Source(msg.data)
-    src.render(outfile="/tmp/graph.pdf", format="pdf")
+    src.render(outfile=PATH, format="pdf")
 
 
 if __name__ == "__main__":
     rospy.init_node("human_graph_viewer", anonymous=True)
     rospy.Subscriber("/humans/graph", String, callback)
+
+    rospy.loginfo(
+        "Ready! Open %s in your favorite PDF viewer to display the graph." % PATH
+    )
     rospy.spin()
