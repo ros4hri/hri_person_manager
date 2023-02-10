ros4hri/hri_person_manager

commit 829ca78430b1101a78ccdebd3d6455c2a73dcbb8

do not re-create nodes when the candidate_match likelihood is 0

src/person_matcher.cpp
@@ -730,6 +730,17 @@ void PersonMatcher::update(Relations relations)
     std::tie(id1, type1, id2, type2, likelihood) = rel;
 
     Node v1 = get_node_by_name(id1, g);
+    Node v2 = get_node_by_name(id2, g);
+
+    if (likelihood <= 0.0)
+    {
+      if (v1 != INEXISTANT_VERTEX && v2 != INEXISTANT_VERTEX)
+      {
+        remove_edge(v1, v2, g);
+      }
+      continue;
+    }
+
 
     if (v1 == INEXISTANT_VERTEX)
     {
@@ -738,19 +749,12 @@ void PersonMatcher::update(Relations relations)
       g[v1].type = type1;
     }
 
-    Node v2 = get_node_by_name(id2, g);
 
     if (v2 == INEXISTANT_VERTEX)
     {
       v2 = add_vertex(g);
       g[v2].name = id2;
       g[v2].type = type2;
-    }
-
-    if (likelihood <= 0.0)
-    {
-      remove_edge(v1, v2, g);
-      continue;
     }
 
     Edge e;

commit c2e6f91af86f72eaac63351c7e8e54538b7e8b74

fix 829ca7843 accidentally prevented new features to be created

src/hri_person_manager.cpp
@@ -284,7 +284,7 @@ public:
           // create a single 'orphan' node. At the end of the next update cycle,
           // this orphan node will be associated to an anonymous_persons if it
           // is not linked to any other node
-          person_matcher.update({ { id1, type1, id2, type2, likelihood } });
+          person_matcher.update({ { id1, type1, id2, type2, 1.0 } });
         }
         break;
 

src/person_matcher.cpp
@@ -751,6 +751,12 @@ void PersonMatcher::update(Relations relations)
       g[v1].type = type1;
     }
 
+    // id1 == id2 -> we a simply creating a new feature; no edge to add
+    if (id1 == id2)
+    {
+      continue;
+    }
+
 
     if (v2 == INEXISTANT_VERTEX)
     {