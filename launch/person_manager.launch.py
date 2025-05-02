b9451032518c0e3b8d0d8413e452bf8499a575d1
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2025-05-02 11:58:27 +0200

    configuration handling: port to PAPS-007

--- launch/person_manager.launch.py
+++ launch/person_manager.launch.py
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
+from launch.actions import EmitEvent, RegisterEventHandler, Shutdown
 from launch.events import matches_action
-from launch.substitutions import LaunchConfiguration
+from launch_pal import get_pal_configuration
 from launch_ros.actions import LifecycleNode
 from launch_ros.events.lifecycle import ChangeState
 from launch_ros.event_handlers import OnStateTransition
@@ -23,35 +23,38 @@ from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
-    param_args = [DeclareLaunchArgument(n, default_value=v, description=d) for n, v, d in [
-        ('match_threshold', '0.5', "Minimum likelihood to associate a face/body/voice to a given "
-                                   "person"),
-        ('reference_frame', 'map', "This should usually be a static frame wrt to the robot"),
-        ('robot_reference_frame', 'base_link', "Reference frame for persons' distance "
-                                               "computation"),
-        ('personal_distance', '1.2', "Person upper distance threshold for personal zone "
-                                     "(the nearest one) (m)"),
-        ('social_distance', '3.6', "Person distance threshold between personal (nearer) and "
-                                   "social (farther) zone (m)"),
-        ('public_distance', '20.', "Person distance threshold between social (nearer) and public "
-                                   "(farther) zone (m)")]]
+    pkg = 'hri_person_manager'
+    namespace = ''
+    node_name = 'hri_person_manager'
+    node_executable = 'hri_person_manager'
 
-    person_manager_node = LifecycleNode(
-        package='hri_person_manager', executable='hri_person_manager', namespace='',
-        name='hri_person_manager',
-        parameters=[{arg.name: LaunchConfiguration(arg.name)} for arg in param_args])
+    ld = LaunchDescription()
+    config = get_pal_configuration(pkg=pkg, node=node_name, ld=ld)
+
+    node = LifecycleNode(
+        package=pkg,
+        executable=node_executable,
+        namespace=namespace,
+        name=node_name,
+        parameters=config["parameters"],
+        remappings=config["remappings"],
+        arguments=config["arguments"],
+        output='both',
+        emulate_tty=True,
+        on_exit=Shutdown()
+    )
 
     configure_event = EmitEvent(event=ChangeState(
-        lifecycle_node_matcher=matches_action(person_manager_node),
+        lifecycle_node_matcher=matches_action(node),
         transition_id=Transition.TRANSITION_CONFIGURE))
 
     activate_event = RegisterEventHandler(OnStateTransition(
-        target_lifecycle_node=person_manager_node, goal_state='inactive',
+        target_lifecycle_node=node, goal_state='inactive',
         entities=[EmitEvent(event=ChangeState(
-            lifecycle_node_matcher=matches_action(person_manager_node),
-            transition_id=Transition.TRANSITION_ACTIVATE))]))
+            lifecycle_node_matcher=matches_action(node),
+            transition_id=Transition.TRANSITION_ACTIVATE))], handle_once=True))
 
-    return LaunchDescription(param_args + [
-        person_manager_node,
-        configure_event,
-        activate_event])
+    ld.add_action(node)
+    ld.add_action(configure_event)
+    ld.add_action(activate_event)
+    return ld
